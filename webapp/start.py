dc8d16623c72522337450f4c1cf10e894763dfdb
Sam Hus <72938443+sam-hus@users.noreply.github.com>
2023-11-01 15:17:54 -0400

    Add files via upload

---
+++ webapp/start.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' in request.files:
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            return render_template('display.html', image_filename=filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
